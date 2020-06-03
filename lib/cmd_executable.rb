97f67daff4a93d3d12bd9019cf7176daef208a99
Toshiki <toshiki.dev@protonmail.ch>
2020-06-03 19:46:35 +0900

    Add template gem 'feature/#1'

---
+++ lib/cmd_executable.rb
@@ -0,0 +1,6 @@
+require "cmd_executable/version"
+
+module CmdExecutable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cmd_executable/version.rb
@@ -0,0 +1,3 @@
+module CmdExecutable
+  VERSION = "0.1.0"
+end
