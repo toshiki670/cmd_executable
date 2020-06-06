1047305146703f3c38c76880c516d2f32386b459
Toshiki <toshiki.dev@protonmail.ch>
2020-06-06 11:41:26 +0900

    Update spec 'feature/#14'

--- spec/cmd_executable_spec.rb
+++ spec/cmd_executable_spec.rb
@@ -9,38 +9,44 @@ RSpec.describe CmdExecutable do
   let(:instance) { Klass.new }
 
   describe '#executable?' do
-    subject { instance }
-    context 'With absolute path' do
-      let(:command) { '/bin/ls' }
-      it { is_expected.to be_executable command }
+    describe 'Success' do
+      subject { instance }
+      context 'With absolute path' do
+        let(:command) { '/bin/ls' }
+        it { is_expected.to be_executable command }
+      end
+      context 'With invalid absolute path' do
+        let(:command) { '/bin/hoge_invalid' }
+        it { is_expected.to_not be_executable command }
+      end
+      context 'Without path' do
+        let(:command) { 'ls' }
+        it { is_expected.to be_executable command }
+      end
+      context 'When invalid command' do
+        let(:command) { 'hoge_invalid' }
+        it { is_expected.to_not be_executable command }
+      end
+      context 'When symbol' do
+        let(:command) { :ls }
+        it { is_expected.to_not be_executable command }
+      end
     end
-    context 'With invalid absolute path' do
-      let(:command) { '/bin/hoge_invalid' }
-      it { is_expected.to_not be_executable command }
-    end
-    context 'Without path' do
-      let(:command) { 'ls' }
-      it { is_expected.to be_executable command }
-    end
-    context 'When invalid command' do
-      let(:command) { 'hoge_invalid' }
-      it { is_expected.to_not be_executable command }
-    end
-    context 'When empty command' do
-      let(:command) { '' }
-      it { is_expected.to_not be_executable command }
-    end
-    context 'When nil command' do
-      let(:command) { nil }
-      it { is_expected.to_not be_executable command }
-    end
-    context 'When no string' do
-      let(:command) { 10 }
-      it { is_expected.to_not be_executable command }
-    end
-    context 'When symbol' do
-      let(:command) { :ls }
-      it { is_expected.to_not be_executable command }
+
+    describe 'Exception' do
+      subject { -> { instance.executable?(command) } }
+      context 'When empty command' do
+        let(:command) { '' }
+        it { is_expected.to raise_error(ArgumentError) }
+      end
+      context 'When nil command' do
+        let(:command) { nil }
+        it { is_expected.to raise_error(ArgumentError) }
+      end
+      context 'When no string' do
+        let(:command) { 10 }
+        it { is_expected.to raise_error(ArgumentError) }
+      end
     end
   end
 end
