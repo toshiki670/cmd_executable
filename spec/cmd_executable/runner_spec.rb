4ee8c19295e6e401d93a7ebe2a60fc4eee984acc
Toshiki <toshiki.dev@protonmail.ch>
2020-06-09 05:10:38 +0900

    Add helper stream 'feature/#18'

--- spec/cmd_executable/runner_spec.rb
+++ spec/cmd_executable/runner_spec.rb
@@ -3,22 +3,6 @@
 require 'spec_helper'
 
 RSpec.describe CmdExecutable::Runner do
-  # rubocop:disable Security/Eval, Metrics/MethodLength
-  def capture(stream)
-    begin
-      stream = stream.to_s
-      binding.eval("$#{stream} = StringIO.new", __FILE__, __LINE__)
-      yield
-    rescue SystemExit => e
-      puts e.status
-    ensure
-      result = binding.eval("$#{stream}", __FILE__, __LINE__).string
-      binding.eval("$#{stream} = #{stream.upcase}", __FILE__, __LINE__)
-    end
-    result
-  end
-  # rubocop:enable Security/Eval, Metrics/MethodLength
-
   let(:instance) { CmdExecutable::Runner.new }
 
   describe '#check' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'cmd_executable/parser'
 require 'cmd_executable/runner'
 require 'cmd_executable/version'
 
+require 'support/stream_capture_helper'
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
@@ -17,4 +19,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include StreamCapture
 end
