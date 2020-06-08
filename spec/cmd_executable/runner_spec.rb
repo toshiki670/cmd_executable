a1a4c92f91990ab9b52f35feb91fb98d1047ef2e
Toshiki <toshiki.dev@protonmail.ch>
2020-06-08 20:33:05 +0900

    Change spec 'feature/#18'

--- spec/cmd_executable/runner_spec.rb
+++ spec/cmd_executable/runner_spec.rb
@@ -1,26 +1,42 @@
 # frozen_string_literal: true
 
 require 'cmd_executable/runner'
+require 'English'
 
 RSpec.describe CmdExecutable::Runner do
+  def capture(stream)
+    begin
+      stream = stream.to_s
+      eval "$#{stream} = StringIO.new"
+      yield
+      result = eval("$#{stream}").string
+    rescue SystemExit
+      result = eval("$#{stream}").string
+    ensure
+      eval("$#{stream} = #{stream.upcase}")
+    end
+
+    result
+  end
+
   let(:instance) { CmdExecutable::Runner.new }
 
   describe '#check' do
     context 'When success' do
-      subject { instance.check('ls') }
-      it do
-        expect { subject }.to raise_error(SystemExit) do |res|
-          expect(res.status).to eq(0)
-        end
-      end
+      subject { capture(:stdout) { CmdExecutable::Runner.start(%w(-c ls)) } }
+      it { is_expected.to match(/OK/) }
+      it { expect(subject.yield_self { $CHILD_STATUS } ).to be_success }
     end
     context 'When error' do
       subject { instance.check('hoge_invalid') }
-      it do
-        expect { subject }.to raise_error(SystemExit) do |res|
-          expect(res.status).to eq(1)
-        end
-      end
+      subject { capture(:stdout) { CmdExecutable::Runner.start(%w(-c hoge_invalid)) } }
+      it { is_expected.to match(/NOT FOUND/) }
+      it { expect(subject.yield_self { $CHILD_STATUS } ).not_to be_success }
     end
+  end
+
+  describe '#version' do
+    subject { capture(:stdout) { CmdExecutable::Runner.start(%w(-v)) } }
+    it { is_expected.to match(/#{CmdExecutable::VERSION}/) }
   end
 end
