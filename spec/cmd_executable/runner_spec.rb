039a487a11659825eba10a432c68427921874391
Toshiki <toshiki.dev@protonmail.ch>
2020-06-08 21:33:53 +0900

    Fix Layout/SpaceInsideParens 'feature/#18'

--- spec/cmd_executable/runner_spec.rb
+++ spec/cmd_executable/runner_spec.rb
@@ -25,13 +25,13 @@ RSpec.describe CmdExecutable::Runner do
     context 'When success' do
       subject { capture(:stdout) { CmdExecutable::Runner.start(%w[-c ls]) } }
       it { is_expected.to match(/OK/) }
-      it { expect(subject.yield_self { $CHILD_STATUS } ).to be_success }
+      it { expect(subject.yield_self { $CHILD_STATUS }).to be_success }
     end
     context 'When error' do
       subject { instance.check('hoge_invalid') }
       subject { capture(:stdout) { CmdExecutable::Runner.start(%w[-c hoge_invalid]) } }
       it { is_expected.to match(/NOT FOUND/) }
-      it { expect(subject.yield_self { $CHILD_STATUS } ).not_to be_success }
+      it { expect(subject.yield_self { $CHILD_STATUS }).not_to be_success }
     end
   end
 
